5c85d73035e77d1ca8f9915b95639315f5b0d30c
emily <emily.a.h@hotmail.com>
2020-10-27 17:39:19 -0400

    Added user as start and end point for routing

--- brewery.js
+++ brewery.js
@@ -51,6 +51,25 @@ function success(position) {
     var latMaps = position.coords.latitude;
     var lonMaps = position.coords.longitude;
     currentLocation = [latMaps, lonMaps];
+
+    // From HERE Maps documentation: 
+    // Define a variable holding SVG mark-up that defines an icon image:
+    var svgMarkup = '<svg width="24" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">Me</text></svg>';
+
+    // Create an icon, an object holding the latitude and longitude, and a marker:
+    var icon = new H.map.Icon(svgMarkup),
+    coords = {lat: latMaps, lng: lonMaps},
+    marker = new H.map.Marker(coords, {icon: icon});
+
+    // Add the marker to the map and center the map at the location of the marker:
+    map.addObject(marker);
+    map.setCenter(coords);
+
     map.setCenter({lat: latMaps, lng: lonMaps});
     map.setZoom(13);
 
@@ -273,6 +292,10 @@ $(".btnSubmit").on("click", function(event) {
 var routingParamArray = [];
 
 $(document).on("click", ".routeButton", function() {
+
+    breweriesClicked.push({lat: currentLocation[0], lng: currentLocation[1]});
+    breweriesClicked.unshift({lat: currentLocation[0], lng: currentLocation[1]});
+
     // Create the parameters for the routing request:
     for (var j = 0; j < breweriesClicked.length - 1; j++ ) {
         var routingParameters = {
