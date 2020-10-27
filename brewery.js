21c9d08bb37b5416367d8fb6b862d66d82119d35
emily <emily.a.h@hotmail.com>
2020-10-27 12:05:21 -0400

    Added routing between breweries

--- brewery.js
+++ brewery.js
@@ -50,7 +50,6 @@ function success(position) {
 		method: "GET"
 
 	})
-
 	.then(function(city) {
 
     console.log(city);
@@ -71,6 +70,7 @@ function getLocation() {
 
 $("#btnLocation").on("click", getLocation());
 
+var locations = [];
 
 function brewery(cityLocale){
 // will get local brewery information based on the city
@@ -87,7 +87,7 @@ var queryURL = "https://api.openbrewerydb.org/breweries?by_city="+cityLocale;
     .then(function(response) {
         console.log(queryURL);
         console.log(response);
-
+        
         // x.innerHTML = "Latitude: " + latLocal+ 
         // "<br>Longitude: " + lngLocal+
         // "<br>City: " + cityLocale;
@@ -102,6 +102,11 @@ var queryURL = "https://api.openbrewerydb.org/breweries?by_city="+cityLocale;
 
             // if (webLinkaddress == "");
             //     webLinkaddress = "Unknown";
+
+            locations[i] = {
+                brewLat: response[i].latitude,
+                brewLon: response[i].longitude
+            }
 
             console.log(webLinkaddress);
 
@@ -119,21 +124,19 @@ var queryURL = "https://api.openbrewerydb.org/breweries?by_city="+cityLocale;
         
             var nameHeader = $("<h4>");
             nameHeader.addClass("header4");
-            nameHeader.attr({id: "name-"+ brewNum});
+            nameHeader.attr({id: "name-" + brewNum});
             nameHeader.css("margin-bottom", "0px");
             nameHeader.text(brewName);
 
             var buttonSpan = $("<span>");
             var breweryBtn = $("<button>");
             breweryBtn.addClass("breweryButton");
-            breweryBtn.attr({id:"Button-" + brewNum});
+            breweryBtn.attr({id: brewNum - 1});
             breweryBtn.text("Add to Map");
 
-            
             buttonSpan.append(breweryBtn);
             nameHeader.append(buttonSpan);
             
-
             var breweryPar1 = $("<p>");
             breweryPar1.addClass("type");
             breweryPar1.attr({id: "typeP-" + brewNum});
@@ -179,3 +182,109 @@ var queryURL = "https://api.openbrewerydb.org/breweries?by_city="+cityLocale;
         }    
     })
 }
+
+var breweriesClicked = [];
+console.log(breweriesClicked);
+
+// Add a marker to the map when a brewery button is clicked:
+$(document).on("click", ".breweryButton", function() {
+
+    // Define a variable holding SVG mark-up that defines an icon image:
+    var svgMarkup = '<svg width="24" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">B</text></svg>';
+
+    // Create an icon, an object holding the latitude and longitude, and a marker:
+    var icon = new H.map.Icon(svgMarkup),
+        coords = {lat: locations[this.id].brewLat, lng: locations[this.id].brewLon},
+        marker = new H.map.Marker(coords, {icon: icon});
+
+    // Add the marker to the map and center the map at the location of the marker:
+    map.addObject(marker);
+    map.setCenter(coords);
+
+    breweriesClicked.push({lat: locations[this.id].brewLat, lng: locations[this.id].brewLon});
+
+    // if two breweries have been clicked, add Calculate Route button
+    if (breweriesClicked.length === 2) {
+        var newBtn = $("<button>");
+        var newSpan = $("<span>");
+
+        newBtn.addClass("routeButton");
+        newBtn.attr({id: "route"});
+        newBtn.text("Calculate Route");
+
+        newSpan.append(newBtn);
+        $("#brewlistHeader").append(newSpan);
+    }
+
+});
+
+// Routing ////////////////////////////////////////////////////////////////////////////////////
+
+var routingParamArray = [];
+
+$(document).on("click", ".routeButton", function() {
+    // Create the parameters for the routing request:
+    for (var j = 0; j < breweriesClicked.length - 1; j++ ) {
+        var routingParameters = {
+            'routingMode': 'fast',
+            'transportMode': 'pedestrian',
+            // The start point of the route:
+            'origin': breweriesClicked[j].lat + "," + breweriesClicked[j].lng,
+            // The end point of the route:
+            'destination': breweriesClicked[j+1].lat + "," + breweriesClicked[j+1].lng,
+            // Include the route shape in the response
+            'return': 'polyline'
+        };
+        routingParamArray.push(routingParameters);
+    }
+    console.log(routingParamArray);
+
+    // Define a callback function to process the routing response:
+    var onResult = function(result) {
+
+        // ensure that at least one route was found
+        if (result.routes.length) {
+            result.routes[0].sections.forEach((section) => {
+                // Create a linestring to use as a point source for the route line
+                let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
+
+                // Create a polyline to display the route:
+                let routeLine = new H.map.Polyline(linestring, {
+                style: { strokeColor: 'blue', lineWidth: 3 }
+                });
+
+                // Create a marker for the start point:
+                let startMarker = new H.map.Marker(section.departure.place.location);
+
+                // Create a marker for the end point:
+                let endMarker = new H.map.Marker(section.arrival.place.location);
+
+                // Add the route polyline and the two markers to the map:
+                map.addObjects([routeLine, startMarker, endMarker]);
+
+                // Set the map's viewport to make the whole route visible:
+                map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
+            });
+        }
+    };
+
+    // Get an instance of the routing service version 8:
+    var router = platform.getRoutingService(null, 8);
+
+    // Call calculateRoute() with the routing parameters,
+    // the callback and an error callback function (called if a
+    // communication error occurs):
+    for (var k = 0; k < breweriesClicked.length - 1; k++) {
+        router.calculateRoute(routingParamArray[k], onResult,
+            function(error) {
+                alert(error.message);
+        });
+    }
+
+    
+});
